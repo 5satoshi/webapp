5e082c4a6c753cd426f4615cde295b9e5dbc311d
Simon Honc <gcp388928@five-satoshi.com>
2025-07-23 13:11:40 +0000

    Fetch error Failed to fetch https://5sats.com/api/openapi.yaml Fetch err

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/production-spec.yaml',
+        destination: 'https://5sats.com/api/openapi.yaml',
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
